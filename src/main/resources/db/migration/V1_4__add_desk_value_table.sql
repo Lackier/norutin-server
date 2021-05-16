7cde1fdc825dca1c23d3b70e1791717caba51417
Daniel <chernov.daniel0@gmail.com>
2021-05-17 02:11:16 +0300

    Revert "task-10: task settings tables, entities"
    This reverts commit fca636fb

--- src/main/resources/db/migration/V1_4__add_desk_value_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "desk_value"
-(
-    "id"        serial       NOT NULL,
-    "desk_id"   integer      NOT NULL,
-    CONSTRAINT "desk_value_pk" PRIMARY KEY ("id")
-);
-
-ALTER TABLE "desk_value"
-    ADD CONSTRAINT "desk_value_fk_desk" FOREIGN KEY ("desk_id") REFERENCES "desk" ("id");

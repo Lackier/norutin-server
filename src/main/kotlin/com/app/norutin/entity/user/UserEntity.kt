414c98b4f1352536679716c96b7dcb844cdfb56b
dchernov <d.chernov@globus-it.com>
2023-08-26 18:39:57 -0600

    скрыл отображение пароля в профиле пользователя

--- src/main/kotlin/com/app/norutin/entity/user/UserEntity.kt
+++ src/main/kotlin/com/app/norutin/entity/user/UserEntity.kt
@@ -7,23 +7,25 @@ import javax.persistence.*
 @Entity
 @Table(name = "users")
 open class UserEntity(
-        id: Int?,
+    id: Int?,
 
-        val username: String,
+    val username: String,
 
-        var password: String,
+    var password: String,
 
-        val phone: String,
+    val phone: String,
 
-        val email: String,
+    val email: String,
 
-        @ManyToMany(fetch = FetchType.EAGER)
-        @JoinTable(name = "user_roles",
-                joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")],
-                inverseJoinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")])
-        var roles: List<RoleEntity?>?,
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")],
+        inverseJoinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")]
+    )
+    var roles: List<RoleEntity?>?,
 
-        @OneToMany
-        @JoinColumn(name = "user_id", referencedColumnName = "id")
-        open val desks: List<DeskEntity>?
+    @OneToMany
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    open val desks: List<DeskEntity>?
 ) : AbstractEntity(id)
